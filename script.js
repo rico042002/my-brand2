131f03eef738438739a27f9159d6d175330afc1e
Rico042002 <rico042002@gmail.com>
2022-02-25 18:12:36 -0600

    commit 4

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", ()=> {
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function toggle() {
+    let header = document.querySelector("header");
+    header.classList.toggle("active");
+}
